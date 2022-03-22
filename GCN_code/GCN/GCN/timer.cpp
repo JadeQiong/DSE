e0bdd3beae47fb55a7178dc7ac54d37f8f5abd99
JadeQiong <volnut@163.com>
2022-03-22 23:44:56 +0800

    feat: timer

--- GCN_code/GCN/GCN/timer.cpp
+++ GCN_code/GCN/GCN/timer.cpp
@@ -2,11 +2,25 @@
 #include "timer.h"
 
 
-timer::timer()
+Timer::Timer()
 {
 }
 
 
-timer::~timer()
+Timer::~Timer()
 {
+}
+
+void Timer::timerStart(timerInstance t) {
+	tmr_t0[t] = std::chrono::high_resolution_clock::now();
+}
+
+float Timer::timerStop(timerInstance t) {
+	float cnt = std::chrono::duration_cast<std::chrono::duration<float>>(std::chrono::high_resolution_clock::now()-tmr_t0[t]).count();
+	tmrSum[t] += cnt;
+	return cnt;
+}
+
+float Timer::timerTotal(timerInstance t) {
+	return tmrSum[t];
 }
--- GCN_code/GCN/GCN/timer.h
+++ GCN_code/GCN/GCN/timer.h
@@ -1,8 +1,33 @@
 #pragma once
-class timer
+#include<chrono>
+#include<vector>
+class Timer
 {
+private:
+
 public:
-	timer();
-	~timer();
+	typedef enum {
+		TMR_TRAIN = 0,
+		TMR_TEST,
+		TMR_MATMUL_FW,
+		TMR_MATMUL_BW,
+		TMR_SPMATMUL_FW,
+		TMR_SPMATMUL_BW,
+		TMR_GRAPHSUM_FW,
+		TMR_GRAPHSUM_BW,
+		TMR_LOSS_FW,
+		TMR_RELU_FW,
+		TMR_RELU_BW,
+		TMR_DROPOUT_FW,
+		TMR_DROPOUT_BW,
+		__NUM_TMR
+	}timerInstance;
+	float tmrSum[__NUM_TMR];
+	std::chrono::time_point<std::chrono::high_resolution_clock> tmr_t0[__NUM_TMR];
+	Timer();
+	~Timer();
+	void timerStart(timerInstance t);
+	float timerStop(timerInstance t);
+	float timerTotal(timerInstance t);
 };
 
